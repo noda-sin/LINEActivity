93c2dd7d64d0929ebb8b24c75ffdec2f071d5f5c
@noda_sin <noda.sin@gmail.com>
2012-12-05 01:31:01 +0900

    ファイル追加

---
+++ LINEActivity/LINEActivity.h
@@ -0,0 +1,12 @@
+//
+//  LINEActivity.h
+//
+//  Created by Noda Shimpei on 2012/12/04.
+//  Copyright (c) 2012年 @noda_sin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LINEActivity : UIActivity
+
+@end
